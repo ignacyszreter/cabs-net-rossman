ca8d2ab680e0d6a648fa83cc16f1990c06ce44a3
Kasia Wróbel <k.wrobel@cabs.example>
2025-09-17 17:15:00 +0100

    test

--- src/CabsTests/Globals.cs
+++ src/CabsTests/Globals.cs
@@ -2,3 +2,4 @@
 global using Assert = NUnit.Framework.Legacy.ClassicAssert;
 global using CollectionAssert = NUnit.Framework.Legacy.CollectionAssert;
 global using FluentAssertions;
+global using NSubstitute;
