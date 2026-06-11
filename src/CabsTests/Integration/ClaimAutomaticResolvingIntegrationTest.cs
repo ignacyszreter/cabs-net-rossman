87b3d9d3472d283f4af2321ffd4423359db651aa
Ignacy Szreter <ignacyszreter@Ignacys-MacBook-Air.local>
2026-06-12 00:57:00 +0100

    Aggregate 3.7 - AwardsAccount z Miles - odkrycie, że mile specjalne to po prostu mile typu non-expiring

--- src/CabsTests/Integration/ClaimAutomaticResolvingIntegrationTest.cs
+++ src/CabsTests/Integration/ClaimAutomaticResolvingIntegrationTest.cs
@@ -93,7 +93,7 @@ public class ClaimAutomaticResolvingIntegrationTest
     Assert.AreEqual(Claim.Statuses.Refunded, claim.Status);
     Assert.AreEqual(Claim.CompletionModes.Automatic, claim.CompletionMode);
     ClientNotificationService.Received(1).NotifyClientAboutRefund(claim.ClaimNo, claim.Owner.Id);
-    await AwardsService.Received(1).RegisterSpecialMiles(claim.Owner.Id, 10);
+    await AwardsService.Received(1).RegisterNonExpiringMiles(claim.Owner.Id, 10);
   }
 
   [Test]
