e17d795af2af829371125faa0b5b87c5c7edae45
Ignacy Szreter <ignacyszreter@Ignacys-MacBook-Air.local>
2026-07-24 00:15:00 +0100

    CQRS 4.4 - DriverTrackingService - obniżenie wysokości testu

--- src/CabsTests/Common/CabsApp.cs
+++ src/CabsTests/Common/CabsApp.cs
@@ -1,6 +1,7 @@
 using System;
 using LegacyFighter.Cabs.Controllers;
 using LegacyFighter.Cabs.DriverReports;
+using LegacyFighter.Cabs.DriverReports.TravelledDistances;
 using LegacyFighter.Cabs.Repository;
 using LegacyFighter.Cabs.Service;
 using Microsoft.AspNetCore.Hosting;
@@ -120,6 +121,9 @@ internal class CabsApp : WebApplicationFactory<Program>
 
   public AddressRepository AddressRepository
     => RequestScope().ServiceProvider.GetRequiredService<AddressRepository>();
+
+  public ITravelledDistanceService TravelledDistanceService
+    => RequestScope().ServiceProvider.GetRequiredService<ITravelledDistanceService>();
 
   public FakeClock Clock => _clock;
 
