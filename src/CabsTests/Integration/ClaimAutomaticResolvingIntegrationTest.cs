d93c2466de6aad8855d24186ffc36f1136f5a320
Ignacy Szreter <ignacyszreter@Ignacys-MacBook-Air.local>
2026-09-04 22:28:00 +0100

    BC/Modularyzacja 9 - Fixtures - testowanie na zachowaniach (z góry) i granulacja klasy Fixtures

--- src/CabsTests/Integration/ClaimAutomaticResolvingIntegrationTest.cs
+++ src/CabsTests/Integration/ClaimAutomaticResolvingIntegrationTest.cs
@@ -3,7 +3,6 @@ using LegacyFighter.Cabs.Entity;
 using LegacyFighter.Cabs.Service;
 using LegacyFighter.CabsTests.Common;
 using Microsoft.Extensions.DependencyInjection;
-using NodaTime;
 
 namespace LegacyFighter.CabsTests.Integration;
 
@@ -12,6 +11,7 @@ public class ClaimAutomaticResolvingIntegrationTest
   private IClaimService ClaimService => _app.ClaimService;
   private IClientNotificationService ClientNotificationService { get; set; } = default!;
   private IDriverNotificationService DriverNotificationService { get; set; } = default!;
+  private IGeocodingService GeocodingService { get; set; } = default!;
   private IAwardsService AwardsService { get; set; } = default!;
   private IAppProperties Properties { get; set; } = default!;
   private Fixtures Fixtures => _app.Fixtures;
@@ -22,6 +22,7 @@ public class ClaimAutomaticResolvingIntegrationTest
   {
     ClientNotificationService = Substitute.For<IClientNotificationService>();
     DriverNotificationService = Substitute.For<IDriverNotificationService>();
+    GeocodingService = Substitute.For<IGeocodingService>();
     AwardsService = Substitute.For<IAwardsService>();
     Properties = Substitute.For<IAppProperties>();
 
@@ -30,6 +31,7 @@ public class ClaimAutomaticResolvingIntegrationTest
       ctx.AddSingleton(ClientNotificationService);
       ctx.AddSingleton(DriverNotificationService);
       ctx.AddSingleton(AwardsService);
+      ctx.AddSingleton(GeocodingService);
       ctx.AddSingleton(Properties);
     });
   }
@@ -43,20 +45,24 @@ public class ClaimAutomaticResolvingIntegrationTest
   [Test]
   public async Task SecondClaimForTheSameTransitWillBeEscalated()
   {
+    _app.StartReuseRequestScope();
     //given
     LowCostThresholdIs(40);
     //and
-    var driver = await Fixtures.ADriver();
+    var pickup = await Fixtures.AnAddress();
+    //and
+    var driver = await Fixtures.ANearbyDriver(GeocodingService, pickup);
     //and
     var client = await Fixtures.AClient(Client.Types.Vip);
     //and
-    var transit = await ATransit(client, driver, 39);
+    var transit = await ATransit(pickup, client, driver, 39);
     //and
     var claim = await Fixtures.CreateClaim(client, transit);
     //and
     claim = await ClaimService.TryToResolveAutomatically(claim.Id);
     //and
     var claim2 = await Fixtures.CreateClaim(client, transit);
+    _app.EndReuseRequestScope();
 
     //when
     claim2 = await ClaimService.TryToResolveAutomatically(claim2.Id);
@@ -77,9 +83,11 @@ public class ClaimAutomaticResolvingIntegrationTest
     //and
     var client = await Fixtures.AClientWithClaims(Client.Types.Vip, 3);
     //and
-    var driver = await Fixtures.ADriver();
+    var pickup = await Fixtures.AnAddress();
     //and
-    var transit = await ATransit(client, driver, 39);
+    var driver = await Fixtures.ANearbyDriver(GeocodingService, pickup);
+    //and
+    var transit = await ATransit(pickup, client, driver, 39);
     //and
     var claim = await Fixtures.CreateClaim(client, transit);
     _app.EndReuseRequestScope();
@@ -105,9 +113,11 @@ public class ClaimAutomaticResolvingIntegrationTest
     //and
     var client = await Fixtures.AClientWithClaims(Client.Types.Vip, 3);
     //and
-    var driver = await Fixtures.ADriver();
+    var pickup = await Fixtures.AnAddress();
     //and
-    var transit = await ATransit(client, driver, 50);
+    var driver = await Fixtures.ANearbyDriver(GeocodingService, pickup);
+    //and
+    var transit = await ATransit(pickup, client, driver, 50);
     //and
     var claim = await Fixtures.CreateClaim(client, transit);
     _app.EndReuseRequestScope();
@@ -135,13 +145,21 @@ public class ClaimAutomaticResolvingIntegrationTest
     //and
     var client = await AClient(Client.Types.Normal);
     //and
-    var driver = await Fixtures.ADriver();
+    var pickup = await Fixtures.AnAddress();
+    //and
+    var driver = await Fixtures.ANearbyDriver(GeocodingService, pickup);
 
     //when
-    var claim1 = await ClaimService.TryToResolveAutomatically((await Fixtures.CreateClaim(client, await ATransit(client, driver, 50))).Id);
-    var claim2 = await ClaimService.TryToResolveAutomatically((await Fixtures.CreateClaim(client, await ATransit(client, driver, 50))).Id);
-    var claim3 = await ClaimService.TryToResolveAutomatically((await Fixtures.CreateClaim(client, await ATransit(client, driver, 50))).Id);
-    var claim4 = await ClaimService.TryToResolveAutomatically((await Fixtures.CreateClaim(client, await ATransit(client, driver, 50))).Id);
+    var claim1 = await ClaimService.TryToResolveAutomatically((await Fixtures.CreateClaim(client, await ATransit(pickup, client, driver, 50))).Id);
+    var claim2 = await ClaimService.TryToResolveAutomatically((await Fixtures.CreateClaim(client, await ATransit(pickup, client, driver, 50))).Id);
+    var claim3 = await ClaimService.TryToResolveAutomatically((await Fixtures.CreateClaim(client, await ATransit(pickup, client, driver, 50))).Id);
+    //and
+    var transit = await ATransit(pickup, client, driver, 50);
+    //and
+    AwardsService.ClearReceivedCalls();
+    //and
+    var claim4 = await ClaimService.TryToResolveAutomatically((await Fixtures.CreateClaim(client, transit)).Id);
+    _app.EndReuseRequestScope();
 
     //then
     Assert.AreEqual(Claim.Statuses.Refunded, claim1.Status);
@@ -157,7 +175,6 @@ public class ClaimAutomaticResolvingIntegrationTest
     ClientNotificationService.Received(1).NotifyClientAboutRefund(claim2.ClaimNo, client.Id);
     ClientNotificationService.Received(1).NotifyClientAboutRefund(claim3.ClaimNo, client.Id);
     AwardsService.ReceivedCalls().Should().BeEmpty();
-    _app.EndReuseRequestScope();
   }
 
   [Test]
@@ -171,11 +188,17 @@ public class ClaimAutomaticResolvingIntegrationTest
     //and
     var client = await Fixtures.AClientWithClaims(Client.Types.Normal, 3);
     //and
-    await Fixtures.ClientHasDoneTransits(client, 12);
+    await Fixtures.ClientHasDoneTransits(client, 12, GeocodingService);
     //and
-    var transit = await ATransit(client, await Fixtures.ADriver(), 39);
+    var pickup = await Fixtures.AnAddress();
+    //and
+    var driver = await Fixtures.ANearbyDriver(GeocodingService, pickup);
+    //and
+    var transit = await ATransit(pickup, client, driver, 39);
     //and
     var claim = await Fixtures.CreateClaim(client, transit);
+    //and
+    AwardsService.ClearReceivedCalls();
     _app.EndReuseRequestScope();
 
     //when
@@ -198,10 +221,15 @@ public class ClaimAutomaticResolvingIntegrationTest
     NoOfTransitsForAutomaticRefundIs(10);
     //and
     var client = await Fixtures.AClientWithClaims(Client.Types.Normal, 3);
+    await Fixtures.ClientHasDoneTransits(client, 12, GeocodingService);
     //and
-    await Fixtures.ClientHasDoneTransits(client, 12);
+    var pickup = await Fixtures.AnAddress();
     //and
-    var claim = await Fixtures.CreateClaim(client, await ATransit(client, await Fixtures.ADriver(), 50));
+    var driver = await Fixtures.ANearbyDriver(GeocodingService, pickup);
+    //and
+    var claim = await Fixtures.CreateClaim(client, await ATransit(pickup, client, driver, 50));
+    //and
+    AwardsService.ClearReceivedCalls();
     _app.EndReuseRequestScope();
 
     //when
@@ -225,11 +253,17 @@ public class ClaimAutomaticResolvingIntegrationTest
     //and
     var client = await Fixtures.AClientWithClaims(Client.Types.Normal, 3);
     //and
-    await Fixtures.ClientHasDoneTransits(client, 2);
+    await Fixtures.ClientHasDoneTransits(client, 2, GeocodingService);
     //and
-    var driver = await Fixtures.ADriver();
+    var pickup = await Fixtures.AnAddress();
     //and
-    var claim = await Fixtures.CreateClaim(client, await ATransit(client, driver, 50));
+    var driver = await Fixtures.ANearbyDriver(GeocodingService, pickup);
+    //and
+    var transit = await ATransit(pickup, client, driver, 50);
+    //and
+    AwardsService.ClearReceivedCalls();
+    //and
+    var claim = await Fixtures.CreateClaim(client, transit);
     _app.EndReuseRequestScope();
 
     //when
@@ -242,9 +276,10 @@ public class ClaimAutomaticResolvingIntegrationTest
     AwardsService.ReceivedCalls().Should().BeEmpty();
   }
 
-  private async Task<Transit> ATransit(Client client, Driver driver, int price) 
+  private async Task<Transit> ATransit(Address pickup, Client client, Driver driver, int price)
   {
-    return await Fixtures.ACompletedTransitAt(price, SystemClock.Instance.GetCurrentInstant(), client, driver);
+    var destination = await Fixtures.AnAddress();
+    return await Fixtures.AJourney(price, client, driver, pickup, destination);
   }
 
   private void LowCostThresholdIs(int price)
